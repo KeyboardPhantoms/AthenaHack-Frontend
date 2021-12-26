f7c632b2ea62d84598b7bf254240da13b3362233
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-26 12:48:40 +0530

    working app fixed the issues

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,51 +1,5 @@
-
-
 const Homepage = () => {
-    return ( <div
-    style={{
-        background: '#2d1674',
-        width: '100%',
-        height: '100vh',
-    }}
-    >
-        <div
-        style={{
-            background: '#b175ff',
-            position: 'relative',
-            width: '100%',
-            height: '35vh',
-        }}>
-            <div
-            style={{
-                //padding: '20rem, 20vh, 0, 0',
-                position: 'absolute',
-                width: '40vh',
-                height: '30vh',
-                margin: '30vh, 20vh, 20vh, 20vh',
-            }}
-            >
-                <img src="https://media.istockphoto.com/vectors/book-icon-vector-id880622992?k=20&m=880622992&s=612x612&w=0&h=UQp48EJNnpw5xB3NFFkr1PmwkHnYFjLeeF9_y9RJhyU=" alt=""
-                width= "100%"
-                height= "100%"
-                style={{}}
-                />
-                
-            </div>
-            
-            <h1
-            style={{
-                position: 'absolute',
-                float: 'right',
-                margin: '0vh, 30vh, 0, 0',
-            }}
-            >Bookie Bits</h1>
-        </div>
-        <div>
-            <div>Genre</div>
-            <div>Authors</div>
-            <div>Published Date</div>
-        </div>
-        Bookie Bits</div> );
-}
- 
+  return <div>Homepage</div>;
+};
+
 export default Homepage;
