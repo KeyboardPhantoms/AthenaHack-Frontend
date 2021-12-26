c9b5ae4f85b7be2c57d47b4f34d8ea7ce8bccd82
codingninja11 <akritisheresth@gmail.com>
2021-12-26 19:02:38 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About/About';
 import Header from './components/Header/Header';
 import Homepage from './components/Homepage/Homepage';
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 import Register from './components/Register/Register';
 import UserData from './components/UserData/UserData';
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/register" element={<Register />} />
         <Route path="/user" element={<UserData />} />
         <Route path="/about" element={<About />} />
